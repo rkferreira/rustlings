0175d64451e38d0d5146413cc14935be494ffff1
Rodrigo Kellermann Ferreira <rkferreira@gmail.com>
2023-04-27 22:18:26 -0300

    errors fase1

--- exercises/error_handling/errors3.rs
+++ exercises/error_handling/errors3.rs
@@ -4,15 +4,19 @@
 // Why not? What should we do to fix it?
 // Execute `rustlings hint errors3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
 
 use std::num::ParseIntError;
 
+//fn main() -> Result<(), ParseIntError> {
 fn main() {
     let mut tokens = 100;
     let pretend_user_input = "8";
+//  let pretend_user_input = "affskdm";
 
-    let cost = total_cost(pretend_user_input)?;
+//  let cost = total_cost(pretend_user_input).unwrap();
+    let cost = total_cost(pretend_user_input).expect("error");
+//  let cost = total_cost(pretend_user_input)?;
 
     if cost > tokens {
         println!("You can't afford that many!");
@@ -20,6 +24,7 @@ fn main() {
         tokens -= cost;
         println!("You now have {} tokens.", tokens);
     }
+//  Ok(())
 }
 
 pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
